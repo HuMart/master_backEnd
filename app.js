822d6860ba62095f2346a877c3e6e9b3287e7e7a
HuMart <manya01@protonmail.com>
2020-05-07 15:46:29 -0400

    testing server

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json());
 // CORS
 
 // RE-ROUTES
-
+// TEST-ROUTE
+app.get('/test', (requ, res) => {
+    return res.status(200).send("<h1>API Working and connecting to the browser</h1>");
+});
 // EXPORT MODULE
 module.exports = app;
 
