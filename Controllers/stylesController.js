227ae65454684b1b62ac7f463167ac50b0b74ca9
HuMart <manya01@protonmail.com>
2020-05-12 12:08:13 -0400

    styles saving in the db

--- Controllers/stylesController.js
+++ Controllers/stylesController.js
@@ -1,10 +1,59 @@
 'use strict'
 
+var validator = require('validator');
+var Style = require('../Models/style');
+
 var stylesController = {
+    // TESTING METHOD
     test: (req, res) => {
         return res.status(200).send({
             message: 'testing styles controller'
         });
+    },
+
+    save: (req, res) => {
+
+
+        try {
+            var params = req.body;
+            var validateTitle = !validator.isEmpty(params.title);
+            var validateContent = !validator.isEmpty(params.content);
+        } catch (err) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'error on the validation, missing data'
+            });
+        }
+
+        if (validateTitle && validateContent) {
+            var style = new Style();
+
+            style.title = params.title;
+            style.content = params.content;
+            style.image = null;
+
+            style.save((err, styleStored) => {
+                if(err || !styleStored){
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'Style not saved'
+                    });
+                }
+                return res.status(200).send({
+                    status: 'success',
+                    style: styleStored
+                });
+            });
+
+        } else {
+            return res.status(200).send({
+                status: 'error',
+                message: 'error on the validation, missing data'
+            });
+        }
+
+
+
     },
 };
 
--- Routes/stylesRoutes.js
+++ Routes/stylesRoutes.js
@@ -3,7 +3,11 @@
 var express = require('express');
 var stylesController = require('../Controllers/stylesController');
 var router = express.Router();
-
+var md_auth = require('../Middlewares/authenticated');
+// TESTING METHODS AND ROUTES
 router.get('/test', stylesController.test);
+// /////////////////////////////////////////////////////////////////////
+
+router.post('/save', md_auth.authenticated, stylesController.save);
 
 module.exports = router;
