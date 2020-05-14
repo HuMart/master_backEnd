97e149be439e6bff06b48f01281183e4c13623ed
HuMart <manya01@protonmail.com>
2020-05-14 11:54:44 -0400

    cors added to app.js

--- app.js
+++ app.js
@@ -3,25 +3,38 @@
 // REQUIRES
 var express = require('express');
 var bodyParser = require('body-parser');
+
 // EXECUTE EXPRESS
 var app = express();
+
 // ROUTER FILES
 var user_routes = require('./Routes/userRoutes');
 var styles_routes = require('./Routes/stylesRoutes');
 var comment_routes = require('./Routes/commentRoutes');
+
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
 // CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // RE-ROUTES
 app.use('/api', user_routes);
 app.use('/api/styles', styles_routes);
 app.use('/api', comment_routes);
+
 // TEST-ROUTE
-app.get('/test', (requ, res) => {
+app.get('/test', (req, res) => {
     return res.status(200).send("<h1>API Working and connecting to the browser</h1>");
 });
+
 // EXPORT MODULE
 module.exports = app;
 
